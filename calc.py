8ca9739ce0e0acc033cab58c92ab789f89893b6e
anishgorakavi787809 <34226544+anishgorakavi787809@users.noreply.github.com>
2023-06-25 18:12:52 -0500

    rejub

---
+++ calc.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from tkinter import messagebox
+
+wdow = tk.Tk()
+
+operator_var = tk.StringVar(wdow,"+")
+
+def do_math():
+    match operator_var.get():
+        case "+":
+            messagebox.showinfo(message=str(first_number_variable.get() + second_number_variable.get()))
+        case "-":
+            messagebox.showinfo(message=str(first_number_variable.get() - second_number_variable.get()))
+        case "*":
+            messagebox.showinfo(message=str(first_number_variable.get() * second_number_variable.get()))
+        case "/":
+            try:
+                messagebox.showinfo(message=str(first_number_variable.get() / second_number_variable.get()))
+            except ZeroDivisionError:
+                messagebox.showerror(message="Hey, you cannot put second box as 0")
+        case _:
+            messagebox.showinfo(message="Somehow, this bug came.")
+#Operators 
+
+add = tk.Radiobutton(wdow,text="+",variable=operator_var,value="+")
+add.grid(row=1,column=1)
+
+sub = tk.Radiobutton(wdow,text="-",variable=operator_var,value="-")
+sub.grid(row=2,column=1)
+
+mul = tk.Radiobutton(wdow,text="*",variable=operator_var,value="*")
+mul.grid(row=3,column=1)
+
+div = tk.Radiobutton(wdow,text="/",variable=operator_var,value="/")
+div.grid(row=4,column=1)
+
+submit_button = tk.Button(wdow,text="Submit!",command=do_math).grid(column=1,row=5)
+#2 number entry
+
+first_number_variable = tk.IntVar(wdow)
+second_number_variable = tk.IntVar(wdow)
+
+first_number_input = tk.Entry(wdow,textvariable=first_number_variable).grid(column=0,row=2)
+second_number_input = tk.Entry(wdow,textvariable=second_number_variable).grid(column=2,row=2)
+wdow.mainloop()
--- eduber.py
+++ eduber.py
@@ -1,18 +1,15 @@
-
 import tkinter as tk
 
-rome = tk.Tk()
 
-for i in range(1,4):
-    print(12 * i)
-    label = tk.Label(rome,text="Testing Testing!",font=("Ariel",str(12 * i),"underline"))
-    label.pack()
+def do_it_again():
+    text.set(text.get() + "and again...")
 
-button_2 = tk.Button(rome, text="Exceptional button")
-button_2.pack()
-button_2["borderwidth"] = 10
-button_2["highlightthickness"] = 10
-button_2["padx"] = 10
-button_2["pady"] = 5
-button_2["underline"] = 1
-rome.mainloop()
+
+window = tk.Tk()
+button = tk.Button(window, text="Go ahead!", command=do_it_again)
+button.pack()
+text = tk.StringVar()
+message = tk.Message(window, textvariable=text, width=400)
+text.set("You did it again... ")
+message.pack()
+window.mainloop()
